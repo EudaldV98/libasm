adeef9b647ee71021e705a2a1bad2a159ff11406
Jan eudald VAQUER <jvaquer@e2r3p1.42.fr>
2020-02-09 20:39:51 +0100

    done read and wirte

--- main.c
+++ main.c
@@ -6,17 +6,20 @@
 /*   By: jvaquer <jvaquer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 17:34:20 by jvaquer           #+#    #+#             */
-/*   Updated: 2020/02/09 18:26:11 by jvaquer          ###   ########.fr       */
+/*   Updated: 2020/02/09 20:38:44 by jvaquer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 size_t  ft_strlen(const char *);
 char	*ft_strcpy(char *dst, const char *src);
 int		ft_strcmp(const char *s1, const char *s2);
-
+ssize_t	ft_write(int fd, const void *buf, size_t count);
+ssize_t	ft_read(int fd, const void *buf, size_t count);
 int     main(int argc, char **argv)
 {
 	char dst[1000];
@@ -29,5 +32,14 @@ int     main(int argc, char **argv)
 
 	printf ("MY CMP		= %d\n", ft_strcmp(argv[1], argv[2]));
 	printf ("TRUE CMP	= %d\n\n", strcmp(argv[1], argv[2]));
+
+	printf ("MY WRITE	= %ld\n", ft_write(1, "hello\n", 6));
+	printf ("TRUE WRITE	= %ld\n\n", write(1, "hello\n", 6));
+	
+	int ret;
+	ret = (int)ft_read(0, dst, 6);
+	printf("ret is %d, read '%.*s'\n", ret, ret, dst);
+	ret = (int)ft_read(0, dst, 6);
+	printf("ret is %d, read '%.*s'\n", ret, ret, dst);
 	return (0);
 }
